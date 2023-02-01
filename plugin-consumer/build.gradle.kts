21d41a48f10ba311844ceea8c95cf94ad07f6d9c
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-02-01 15:30:54 +0100

    [V-2] Added plugin-consumer

---
+++ plugin-consumer/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins{
+    //id("com.poisonedyouth.countLines").version("1.0.0")
+    id ("org.jetbrains.kotlin.jvm").version("1.8.0")
+}
+
+repositories{
+    mavenCentral()
+}
+
+//tasks.countLines {
+//    println("Configure Task")
+//    fileTypes = listOf("kt", "kts")
+//
+//}
+
+
+
+
+
+
+
+
---
+++ plugin-consumer/src/main/kotlin/com.poisonedyouth/Main.kt
@@ -0,0 +1,5 @@
+package com.poisonedyouth
+
+fun main(){
+    println("Hello World!")
+}
